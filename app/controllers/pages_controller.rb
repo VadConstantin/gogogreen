7fb3f02cb8ce2c5eda14c7a257af66c4bf07fc35
Vadim Constantin <vad.constantin@gmail.com>
2022-11-25 18:51:46 +0100

    good redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
   def gogo_redirect
-    redirect_to "https://gogogreen.fr/", allow_other_host: true
+    redirect_to "https://gogogreen.fr/music-for-plants/", allow_other_host: true
   end
 end
