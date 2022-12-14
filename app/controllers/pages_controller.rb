ef2a46f6cd625d9b1f8eb7dc06093800a698ab53
Vadim Constantin <vad.constantin@gmail.com>
2022-12-14 21:42:05 +0100

    changed redirection address

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
   def gogo_redirect
-    redirect_to "https://gogogreen.fr/music-for-plants/", allow_other_host: true
+    redirect_to "https://open.spotify.com/album/1j3EcPH3RGXs0R8Gtt378y?si=3HTfIPSHSLCf0UmkXNCVRA", allow_other_host: true
   end
 end
